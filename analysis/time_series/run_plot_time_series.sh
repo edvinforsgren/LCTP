888e14f8e070307655fe1049f6f5f14a7b71b1af
Edvin Forsgren <edvin.forsgren@umu.se>
2025-05-01 10:36:46 +0000

    Fixed a typo: dino16->dino14

--- analysis/time_series/run_plot_time_series.sh
+++ analysis/time_series/run_plot_time_series.sh
@@ -19,7 +19,7 @@ time:
     hours2: [46, 48, 50, 52, 54]
 data:
     cell: "$cell"
-    path: "dino_feature_extraction/data/$cell/dino_vitb16_features/$cell-median_well_features.parquet.gzip"
+    path: "dino_feature_extraction/data/$cell/dino_vitb14_features/$cell-median_well_features.parquet.gzip"
     save_path: 'analysis/pred_data/$cell-ts/'
 EOL
 echo "Running Time series plotting for $cell with config file: $config_file"
